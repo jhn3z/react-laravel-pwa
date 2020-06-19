0d7624689b678b707a8961ee54d47fc8adbb0f42
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-06-19 23:12:55 +0530

    added email erification for user

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
-  
+  	Route::get('signup/activate/{token}', 'AuthController@signupActivate');
+  	
     Route::group([
       'middleware' => 'auth:api'
     ], function() {
