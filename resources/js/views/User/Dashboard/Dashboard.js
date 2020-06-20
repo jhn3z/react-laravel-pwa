8b13b31e16ce7a5f136bfb3eff14330ba46d5023
jhn3z <>
2020-06-20 19:33:33 +0530

    handed dashboard view

--- resources/js/views/User/Dashboard/Dashboard.js
+++ resources/js/views/User/Dashboard/Dashboard.js
@@ -1,49 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PhoneIcon from '@material-ui/icons/Phone';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import HomeIcon from '@material-ui/icons/Home'
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import HelpIcon from '@material-ui/icons/Help';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import ThumbDown from '@material-ui/icons/ThumbDown';
+import ThumbUp from '@material-ui/icons/ThumbUp';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import FaceIcon from '@material-ui/icons/Face';
+import Timeline from './Timeline';
+import Friends from './Friends';
+import Profile from './Profile';
+import UploadNew from './UploadNew';
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-import React, {Component} from 'react'
-import Header from '../../../components/Header/Header';
-import Footer from '../../../components/Footer/Footer';
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: false,
-      user: {}
-    }
-  }
-// check if user is authenticated and storing authentication data as states if true
-  componentWillMount() {
-    let state = localStorage["appState"];
-    if (state) {
-      let AppState = JSON.parse(state);
-      console.log(AppState);
-      this.setState({ isLoggedIn: AppState.isLoggedIn, user: AppState.user });
-    }
-  }
-// 4.1
-render() {
-    return (
-      <div>
-        <Header userData={this.state.user} userIsLoggedIn={this.state.isLoggedIn}/>
-          <span>Whatever normally goes into the user dasboard page; the table below for instance</span> <br/>
-          <table className="table table-striped">
-            <tbody>
-              <tr>
-                <th scope="row ">User Id</th>
-                <td>{this.user.id}</td>
-              </tr>
-              <tr>
-                <th scope="row ">Full Name</th>
-                <td>{this.user.first_name}</td>
-              </tr>
-              <tr>
-                <th scope="row ">Email</th>
-                <td>{this.user.email}</td>
-              </tr>
-            </tbody>
-          </table>
-        <Footer/>
-      </div>
-      )
-    }
-  }
-export default Home
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-prevent-tabpanel-${index}`}
+      aria-labelledby={`scrollable-prevent-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-prevent-tab-${index}`,
+    'aria-controls': `scrollable-prevent-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonPrevent() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="off"
+          aria-label="scrollable prevent tabs example"
+        >
+          <Tab icon={<HomeIcon />} aria-label="favorite" {...a11yProps(1)} />
+          <Tab icon={<FaceIcon />} aria-label="phone" {...a11yProps(0)} />
+          <Tab icon={<PersonPinIcon />} aria-label="person" {...a11yProps(2)} />
+          <Tab icon={<AddCircleOutlineIcon />} aria-label="help" {...a11yProps(3)} />
+          <Tab icon={<PowerSettingsNewIcon />} aria-label="shopping" {...a11yProps(4)} />
+ 
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Timeline />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Friends />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Profile />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <UploadNew />
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Logout
+      </TabPanel>
+    </div>
+  );
+}
